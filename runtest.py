09535766d0a7ae2e5d6b38aa5dc0141ed7bde954
Sagar Karandikar <karandikarsagar@gmail.com>
2014-06-23 04:36:41 +0000

    improved testing script

--- runtest.py
+++ runtest.py
@@ -7,10 +7,23 @@ import sys
 t = os.listdir("tests/riscv/")
 newT = []
 
+successes = []
+fails = []
+
 for x in t:
     if "rv64ui" in x:
         newT.append(x)
 
 for x in newT:
     print(x + ": ",end="",flush=True)
-    subprocess.call("./mips64-linux-user/qemu-mips64 " + "tests/riscv/"+x, shell=True)
+    o = subprocess.Popen("./mips64-linux-user/qemu-mips64 " + "tests/riscv/"+x, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).communicate()[0]
+    print(o)
+    if "SUCCESS" in str(o):
+        successes.append(x)
+    else:
+        fails.append(x)
+    
+print("Passing (" + str(len(successes)) + "):")
+print(successes)
+print("Failing (" + str(len(fails)) +"):")
+print(fails)
