1bf6443f148bec89b22468ee299d7f685e78c483
Sagar Karandikar <karandikarsagar@gmail.com>
2014-06-23 02:32:34 +0000

    fixed immediate extraction from stores
    -note that in gdb, seeing segfaults when qemu is executing translated
    store code is normal (this is how qemu handles self-modification)

--- target-mips/translate.c
+++ target-mips/translate.c
@@ -892,7 +892,7 @@ static void gen_store(DisasContext *ctx, uint32_t opc,
 
 
 #define GET_B_IMM(inst)              ((int16_t)((((inst >> 25) & 0x3F) << 5) | ((((int32_t)inst) >> 31) << 12) | (((inst >> 8) & 0xF) << 1) | (((inst >> 7) & 0x1) << 11)))  /* THIS BUILDS 13 bit imm (implicit zero is tacked on here), also note that bit #12 is obtained in a special way to get sign extension */
-#define GET_STORE_IMM(inst)           ((int16_t)((((int32_t)inst) >> 20) | ((inst >> 7) & 0x1F)))
+#define GET_STORE_IMM(inst)           ((int16_t)(((((int32_t)inst) >> 25) << 5) | ((inst >> 7) & 0x1F)))
 
 static void decode_opc (CPUMIPSState *env, DisasContext *ctx)
 {
