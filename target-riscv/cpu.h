b3ea3fd2b7578d22cf44f4f21c0658bb6b3fff76
Sagar Karandikar <sagark@eecs.berkeley.edu>
2016-09-15 08:53:48 -0700

    riscv: cleanup - move riscv-defs.h into cpu.h

--- target-riscv/cpu.h
+++ target-riscv/cpu.h
@@ -12,7 +12,19 @@
 #define ALIGNED_ONLY
 
 #include "qemu-common.h"
-#include "riscv-defs.h"
+
+// QEMU addressing/paging config
+#define TARGET_PAGE_BITS 12 // 4 KiB Pages
+#if defined(TARGET_RISCV64)
+#define TARGET_LONG_BITS 64 // this defs TCGv as TCGv_i64 in tcg/tcg-op.h
+#define TARGET_PHYS_ADDR_SPACE_BITS 50
+#define TARGET_VIRT_ADDR_SPACE_BITS 39
+#elif defined(TARGET_RISCV32)
+#define TARGET_LONG_BITS 32 // this defs TCGv as TCGv_i64 in tcg/tcg-op.h
+#define TARGET_PHYS_ADDR_SPACE_BITS 34
+#define TARGET_VIRT_ADDR_SPACE_BITS 32
+#endif
+
 #include "exec/cpu-defs.h"
 #include "fpu/softfloat.h"
 
