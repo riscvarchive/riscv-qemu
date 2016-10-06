e01969db83f7873186aab298e03f78ccfd133dd7
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2016-10-06 10:48:18 +0200

    target-riscv: Optimize MULHSU not requiring a helper anymore
    this fixes the instruction also for 32 bit hosts.
    
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- target-riscv/helper.h
+++ target-riscv/helper.h
@@ -3,10 +3,6 @@ DEF_HELPER_2(raise_exception, noreturn, env, i32)
 DEF_HELPER_1(raise_exception_debug, noreturn, env)
 DEF_HELPER_3(raise_exception_mbadaddr, noreturn, env, i32, tl)
 
-#if defined(TARGET_RISCV64)
-DEF_HELPER_FLAGS_3(mulhsu, TCG_CALL_NO_RWG_SE, tl, env, tl, tl)
-#endif
-
 /* Floating Point - fused */
 DEF_HELPER_FLAGS_5(fmadd_s, TCG_CALL_NO_RWG, i64, env, i64, i64, i64, i64)
 DEF_HELPER_FLAGS_5(fmadd_d, TCG_CALL_NO_RWG, i64, env, i64, i64, i64, i64)
