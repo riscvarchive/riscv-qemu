70271fc6f45504132db8bd148ba659f7e322bca7
Michael Clark <mjc@sifive.com>
2018-03-04 13:27:37 +1300

    RISC-V - Remove unused class definitions from machines

--- include/hw/riscv/spike.h
+++ include/hw/riscv/spike.h
@@ -19,22 +19,11 @@
 #ifndef HW_SPIKE_H
 #define HW_SPIKE_H
 
-#define TYPE_RISCV_SPIKE_V1_09_1_BOARD "riscv.spike_v1_9.1"
-#define TYPE_RISCV_SPIKE_V1_10_0_BOARD "riscv.spike_v1_10"
-
-#define SPIKE(obj) \
-    OBJECT_CHECK(SpikeState, (obj), TYPE_RISCV_SPIKE_BOARD)
-
 typedef struct {
-    /*< private >*/
-    SysBusDevice parent_obj;
-
-    /*< public >*/
     RISCVHartArrayState soc;
     void *fdt;
     int fdt_size;
 } SpikeState;
-
 
 enum {
     SPIKE_MROM,
