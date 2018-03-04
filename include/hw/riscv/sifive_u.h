91dbd4e6be0475673eb44e06612f28ada5bd9bc4
Michael Clark <mjc@sifive.com>
2018-03-04 13:27:37 +1300

    RISC-V - Remove unused class definitions from machines

--- include/hw/riscv/sifive_u.h
+++ include/hw/riscv/sifive_u.h
@@ -19,16 +19,7 @@
 #ifndef HW_SIFIVE_U_H
 #define HW_SIFIVE_U_H
 
-#define TYPE_SIFIVE_U "riscv.sifive_u"
-
-#define SIFIVE_U(obj) \
-    OBJECT_CHECK(SiFiveUState, (obj), TYPE_SIFIVE_U)
-
 typedef struct SiFiveUState {
-    /*< private >*/
-    SysBusDevice parent_obj;
-
-    /*< public >*/
     RISCVHartArrayState soc;
     DeviceState *plic;
     void *fdt;
